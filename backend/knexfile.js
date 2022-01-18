b168be9a004e41e54b61b2d5c6a6da70f095bfb1
LuizReis-dev <reisluiz05@gmail.com>
2022-01-18 20:56:22 -0300

    Criando projeto e configurando a estrutura do backend

---
+++ backend/knexfile.js
@@ -0,0 +1,38 @@
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'projeto_ecommerce',
+      user:     'postgres',
+      password: '1234'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    },
+    seed: {
+      directory: '.data/seeds'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
