2f71f97e59cdcc764ada9214df94eb88d507e472
LuizReis-dev <reisluiz05@gmail.com>
2022-02-01 20:46:11 -0300

    creating funcionality of get all products (paginated)

---
+++ backend/src/controllers/ProductsController.js
@@ -0,0 +1,36 @@
+const knex = require('../../database/connection');
+
+
+class ProducstController {
+
+    async getAll(req, res) {
+        try {
+            const page = req.query.page || 1;
+            const size = req.query.size || 20;
+            const offset = size * (page - 1);
+
+            const productsDb = await knex('products')
+                .select('*')
+                .limit(size)
+                .offset(offset);
+
+            const [productsQuantity] = await knex('products')
+                .count('id');
+
+            const pagination = {
+                result: productsDb,
+                actualPage: parseInt(page),
+                size: parseInt(size),
+                quantityProducts: parseInt(productsQuantity.count),
+                totalPages: Math.ceil(productsQuantity.count / size),
+                items: productsDb.lenght
+            }
+
+            return res.json(pagination)
+        } catch (error) {
+            return res.status(500).json({ message: 'Ocorreu um erro inesperado!', error: err.message });
+        }
+    }
+}
+
+module.exports = ProducstController;
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const auth = require('./middlewares/authMiddleware');
 const router = express.Router();
 
 const UsersController = require('./controllers/UsersController');;
 const usersController = new UsersController();
 
+const ProducstController = require('./controllers/ProductsController');
+const productsController = new ProducstController();
+
 router.post('/signup', usersController.signup);
 router.post('/login', usersController.login);
+
+router.get('/products', auth(productsController.getAll));
 module.exports = router;
